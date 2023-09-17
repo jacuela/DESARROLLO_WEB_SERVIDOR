88d93e003e55643babd983d3aae639b962613868
jacuela <jacuela@gmail.com>
2023-09-17 12:16:55 +0200

    cambio mensaje

--- ejercicios/01_holamundografico.php
+++ ejercicios/01_holamundografico.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Holamundografico.php
  *
@@ -9,25 +10,28 @@
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilos.css" title="Color">
-    <title>Holamundografico</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estilos.css" title="Color">
+  <title>Holamundografico</title>
 </head>
+
 <body>
   <header>
-    <h1>Hola mundo grafico</h1>
+    <h1>Hola mundo grafico con color de fondo</h1>
   </header>
   <?php
 
-    print "<p>Hola Mundo</p>\n";
+  print "<p>Hola Mundo</p>\n";
 
-  ?>    
+  ?>
 
   <footer>
     <hr>
     <p>Autor: Juan Antonio Cuello</p>
   </footer>
 </body>
+
 </html>
