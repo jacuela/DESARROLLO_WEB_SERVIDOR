40db6514ca48645a5d5b337f58ab648601f60324
jacuela <jacuela@gmail.com>
2023-09-18 22:35:11 +0200

    ejercicio02

--- ejercicios/01_holamundografico.php
+++ ejercicios/01_holamundografico.php
@@ -10,7 +10,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/estilos.css" title="Color">
-  <title>Holamundografico</title>
+  <title>01_Holamundografico</title>
 </head>
 
 <body>
---
+++ ejercicios/02_saludar.php
@@ -0,0 +1,43 @@
+<?php
+//  02_saludar.php
+//  Hacer un programa que usando una función saludar($nombre),
+//  haga un saludo. La función la definiremos en el mismo archivo
+//  @author Cuello
+?>
+
+<?php
+function saludar($nombre = "usuario")
+{
+    print "Hola $nombre"; 
+    
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css" title="Color">
+    <title>02_saludar</title>
+</head>
+
+<body>
+    <header>
+        <h1>Saludar con función</h1>
+    </header>
+    <main>
+        <?php
+        //Si no sale en negrita, ir al inspector web. PISTA: la función debe devolver el valor.
+        print "<p><strong>" . saludar("Juanico") . "</strong></p>";
+        ?>
+    </main>
+    <footer>
+        <hr>
+        <p>Autor: Juan Antonio Cuello</p>
+    </footer>
+</body>
+
+</html>
