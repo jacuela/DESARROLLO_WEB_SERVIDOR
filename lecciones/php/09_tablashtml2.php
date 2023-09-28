283f0513814c393734ed566095bd7b60c4a86b21
jacuela <jacuela@gmail.com>
2023-09-29 00:42:14 +0200

    lecciones php ejer9

---
+++ lecciones/php/09_tablashtml2.php
@@ -0,0 +1,53 @@
+<?php
+$a = [
+    [
+        'Nombre' => 'Mauro',
+        'Apellido' => 'Chojrin',
+        'Correo' => 'mauro.chojrin@leewayweb.com',
+    ],
+    [
+        'Nombre' => 'Alberto',
+        'Apellido' => 'Loffatti',
+        'Correo' => 'aloffatti@hotmail.com',
+    ],
+    [
+        'Nombre' => 'Foo',
+        'Apellido' => 'Bar',
+        'Correo' => 'foo_bar@example.com',
+    ]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<table border="1">
+    <?php
+    foreach ($a as $r) {
+    ?>
+        <tr>
+            <?php
+            foreach ($r as $v) {
+            ?>
+                <td><?php echo $v; ?></td>
+            <?php
+            }
+            ?>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
