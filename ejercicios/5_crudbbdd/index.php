13cd8d3b341243faea0ff6cef49c8d6760379a07
jacuela <jacuela@gmail.com>
2024-01-19 12:15:56 +0100

    5crud

---
+++ ejercicios/5_crudbbdd/index.php
@@ -0,0 +1,27 @@
+<?php
+require_once("funciones.php");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>BBDD - Borrar y crear</title>
+</head>
+
+<body>
+    <?php
+    cabecera("Inicio", MENU_PRINCIPAL);
+    ?>
+    <main>
+    </main>
+
+    <?php
+    pie();
+    ?>
+</body>
+</html>
